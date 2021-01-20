7a11490b0426731e2ac9dc4871a9b3a5ff75979a
AKSHAY HIRAPARA <46915564+AkshHirpara@users.noreply.github.com>
2021-01-20 21:15:42 +0100

    Update trends.py

--- trends.py
+++ trends.py
@@ -22,14 +22,15 @@ class Trends:
         pytrend.build_payload(kw_list=[term])
         region_wise = pytrend.interest_by_region()
         top10 = region_wise.sort_values(by=term, ascending=False).head(10)
-        top10 = pd.DataFrame.to_dict(top10)
+#         top10 = pd.DataFrame.to_dict(top10)
+        top10 = st.bar_chart(top10)
         related_queries = pytrend.related_queries()
         related_queries = pd.DataFrame(related_queries[term]['rising'].sort_values(by="value", ascending=False))
 #     stopwords = stoplists.gtrends_stop_words
 #     remove_words = [word for word in related_queries['query'] if word in stopwords]
 #     related_queries = related_queries[~related_queries['query'].isin(remove_words)]
         related_queries = pd.DataFrame.to_dict(related_queries)
-        return {"top10": top10, "related_queries": related_queries}
+        return {"top10": top10}
 
 
 def main():
